ba06442b12616bd6afa6630474b17898919e272b
med <mederic.munier@gmail.com>
2013-05-02 22:06:58 +0800

    page de listage des produits

---
+++ src/com/lady/beans/ListerProduitsBackingBean.java
@@ -0,0 +1,32 @@
+package com.lady.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.lady.dao.ProduitDao;
+import com.lady.entities.Produit;
+
+@ManagedBean( name = "listerProduitsBean" )
+@ViewScoped
+public class ListerProduitsBackingBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private List<Produit>     produits;
+
+    @EJB
+    private ProduitDao        produitDao;
+
+    @PostConstruct
+    public void init() {
+        produits = produitDao.lister();
+    }
+
+    public List<Produit> getProduits() {
+        return produits;
+    }
+}
