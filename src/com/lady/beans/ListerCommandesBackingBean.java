9a15d518f798832253f2bdcd38ac04fbb4ab7110
mederic <mederic.munier@gmail.com>
2013-05-03 10:51:43 +0800

    page de listage des commandes et conversion UTF-8 <-> ASCII des Properties

---
+++ src/com/lady/beans/ListerCommandesBackingBean.java
@@ -0,0 +1,32 @@
+package com.lady.beans;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import com.lady.dao.CommandeDao;
+import com.lady.entities.Commande;
+
+@ManagedBean( name = "listerCommandesBean" )
+@ViewScoped
+public class ListerCommandesBackingBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private List<Commande>    commandes;
+
+    @EJB
+    private CommandeDao       commandeDao;
+
+    @PostConstruct
+    public void init() {
+        commandes = commandeDao.lister();
+    }
+
+    public List<Commande> getCommandes() {
+        return commandes;
+    }
+}
